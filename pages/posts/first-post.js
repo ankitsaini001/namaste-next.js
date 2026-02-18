7a76c9cf55ddb85020fcb2e262d58e5140f77ae4
ankitsaini001 <ankitsaini594@gmail.com>
2026-02-18 22:48:25 +0530

    Layout Component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,16 @@
+import Layout from '../../components/layout';
 import Link from "next/link";
+
 export default function FirstPost() {
-  return (
-    <>
+
+    return (
+    <Layout>
     <h1>First Post</h1>
     <h2>
         <Link href='/'>Back to Home</Link>
+        {/* https://nextjs.org/learn/pages-router/navigate-between-pages-client-side */}
     </h2>
-    </>
+    </Layout>
   );
+
 }
